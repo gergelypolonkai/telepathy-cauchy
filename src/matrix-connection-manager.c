c884983d321ad621b946420a7c5b2777864decb2
Gergely Polonkai <gergely.polonkai@ericsson.com>
2015-12-09 13:09:56 +0100

    Fix license headers

--- src/matrix-connection-manager.c
+++ src/matrix-connection-manager.c
@@ -1,18 +1,18 @@
 /*
- * This file is part of telepathy-matrix.
+ * This file is part of telepathy-matrix
  *
  * telepathy-matrix is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
  * telepathy-matrix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with telepathy-matrix.  If not, see
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with telepathy-matrix. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
--- src/matrix-connection.h
+++ src/matrix-connection.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of telepathy-matrix
+ *
+ * telepathy-matrix is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of * the License, or (at your option) any later version.
+ *
+ * telepathy-matrix is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with telepathy-matrix. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __MATRIX_CONNECTION_H__
 #define __MATRIX_CONNECTION_H__
 
